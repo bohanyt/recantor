06e2947974f9932c484ddc2196cb8d0ad2ac51bd
bohan <136550677+bohanyt@users.noreply.github.com>
2026-09-09 09:52:00 +0700

    Fix Alembic import ordering

--- apps/api/alembic/env.py
+++ apps/api/alembic/env.py
@@ -1,8 +1,8 @@
 from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from alembic import context
 from recantor.db import Base
 from recantor.settings import get_settings
 
